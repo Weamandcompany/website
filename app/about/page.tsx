249669514d99bd1b5aeaceea4c5e927842c72dc3
Dammyton <owolabidamilola98@gmail.com>
2023-06-24 00:20:57 +0100

    project setup

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: " About | Weam",
+};
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
