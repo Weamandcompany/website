2dfd236def1333fbf2acdcc1060b45f2dd6847af
afuwape sunday <afuwapesunday12@gmail.com>
2023-06-29 15:26:43 +0100

    about fix

--- components/about/HeroSection.tsx
+++ components/about/HeroSection.tsx
@@ -12,13 +12,12 @@ const HeroSection = () => {
         <div className="bg-wdark rounded-none md:rounded-3xl pt-[100px] md:pt-[74px] pb-16 px-4 xs:px-5 sm:px-6 md:px-4 lg:px-14">
           <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6 lg:gap-x-14 md:items-center lg:items-start">
             <div className="">
-              <Fade direction="up">
+              <Slide duration={700} triggerOnce={true}>
                 <h3 className="text-white text-center md:text-left font-nunito font-extrabold lg:text-[40px] text-[25px] leading-[1.4] mb-4">
                   An Excellence Powered by Innovation.
                 </h3>
-              </Fade>
-              <Fade direction="up">
-                <p className="font-nunito text-[13px] text-center md:text-left leading-loose sm:text-sm sm:leading-[210%] text-white font-normal">
+
+                <p className="font-nunito text-[14px] text-center md:text-left leading-loose sm:text-[15px] sm:leading-[2.5] text-white font-normal">
                   Welcome to Weam and Company Limited, where innovation thrives and solutions come
                   to life. With our team of experts and a passion for excellence, we push boundaries
                   and deliver remarkable outcomes. Harnessing cutting-edge technologies and creative
@@ -26,7 +25,7 @@ const HeroSection = () => {
                   Weam and Company Limited and let us inspire the solutions you&apos;ve been
                   searching for.
                 </p>
-              </Fade>
+              </Slide>
               <div className="flex justify-center md:justify-start md:mb-0 mb-10">
                 <a
                   href="#"
