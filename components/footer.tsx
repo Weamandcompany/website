6d8fcb5777d22874439b1a074ba6984a407b5088
Afuwape sunday <afuwapesunday12@gmail.com>
2023-07-20 20:35:36 +0100

    edited DMT footer designs

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { Icon } from '@iconify/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import Footernav from './footernav';
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Footernav from "./footernav";
 
 const Footer = () => {
   return (
@@ -12,47 +12,35 @@ const Footer = () => {
       <Footernav />
       <div className="bg-[#131944] p-8  lg:mx-32 mx-5 lg:mb-[3.5rem] lg:mt-[2.5rem] my-6 mb-10 rounded-[4px] lg:flex items-center justify-between font-poppins text-white gap-24">
         <div className="lg:flex items-start gap-6 lg:w-[85%]">
-          {' '}
+          {" "}
           <Image
-            src={'/images/logowhite.svg'}
+            src={"/images/logo-lg.png"}
             height={0}
             width={0}
             className="h-[80px] w-auto"
             alt="logo"
           />
-          <p className="font-roboto text-[#FFFFFF] text-xs leading-6 lg:w-[65%]">
-            Weam and Company Limited, established in 1985, is an ISO 9001:2015
-            and COREN certified Engineering, Procurement, Construction,
-            Installation and Commissioning (EPCIC) company, providing a diverse
-            range of client-focused and cost-effective engineering solutions in
+          <p className="font-roboto text-white/75 text-[13px] leading-6 lg:w-[65%]">
+            Weam and Company Limited, established in 1985, is an ISO 9001:2015 and COREN certified
+            Engineering, Procurement, Construction, Installation and Commissioning (EPCIC) company,
+            providing a diverse range of client-focused and cost-effective engineering solutions in
             the oil, gas and manufacturing industries
           </p>
         </div>
         <div className="flex items-center gap-6 lg:w-[15%] lg:mt-0 mt-8">
-          Find us on:{' '}
-          <Link
-            href={'https://ng.linkedin.com/company/weam-and-company'}
-            target="_blank"
-          >
+          Find us on:{" "}
+          <Link href={"https://ng.linkedin.com/company/weam-and-company"} target="_blank">
             <Icon
-              icon={'ri:linkedin-fill'}
+              icon={"ri:linkedin-fill"}
               className="text-[#E47B0E] p-[7px] bg-white h-[32px] w-[32px] rounded-full"
             />
           </Link>
         </div>
       </div>
-      <div className="bg-[#131944] lg:flex grid justify-center items-center lg:text-left text-center lg:px-32 px-10 py-4 text-white font-poppins">
-        <p className="text-[12px] lg:order-1 order-2 text-center">
-          © Copyright {new Date().getFullYear()}. All Right Reserved
+      <div className="bg-[#131944] lg:flex grid justify-center items-center lg:text-left text-center lg:px-32 px-10 py-4 text-white/75 font-poppins">
+        <p className="text-sm lg:order-1 order-2 text-center">
+          &copy; Copyright {new Date().getFullYear()} Weam and Company Limited.
         </p>
-        {/* <div className="flex items-center gap-12 justify-center lg:order-2 order-1">
-          <Link href={"/privacy-policy"} className="text-[12px] text-[Orange]">
-            Privacy Policy{" "}
-          </Link>
-          <Link href={"/terms-and-conditions"} className="text-[12px]">
-            Terms & Conditions{" "}
-          </Link>
-        </div> */}
       </div>
     </div>
   );
--- components/footernav.tsx
+++ components/footernav.tsx
@@ -1,60 +1,54 @@
-import { quickLinks, services } from '@/utils/constants';
-import { Icon } from '@iconify/react';
-import Link from 'next/link';
-import React from 'react';
+import { quickLinks, services } from "@/utils/constants";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+import React from "react";
 
 const Footernav = () => {
   return (
-    <div className="lg:mx-32 mx-5 pt-12 grid lg:grid-cols-3 grid-cols-1 space-y-4 text-white">
+    <div className="lg:mx-32 mx-5 pt-20 grid lg:grid-cols-3 grid-cols-1 text-white/75">
       <div>
-        <h6 className="font-bold mb-6">QUICK LINKS</h6>
+        <h6 className="font-bold !text-white mb-6">QUICK LINKS</h6>
         {quickLinks?.map((item, i) => (
           <Link
             key={i}
             href={item?.link}
-            className="flex items-center gap-3 mb-4 text-sm hover:underline"
-          >
+            className="flex items-center gap-3 mb-4 text-[15px] font-normal hover:underline">
             <Icon icon="ph:arrow-right-bold" /> {item?.name}
           </Link>
         ))}
-      </div>{' '}
+      </div>{" "}
       <div>
-        <h6 className="font-bold mb-6">OUR EXPERTISE</h6>
+        <h6 className="font-bold !text-white mb-6">OUR EXPERTISE</h6>
         {services?.map((item, i) => (
           <Link
             key={i}
             href={item?.link}
-            className="flex items-center gap-3 mb-4 text-sm hover:underline"
-          >
+            className="flex items-center gap-3 mb-4 text-[15px] font-normal hover:underline">
             <Icon icon="ph:arrow-right-bold" /> {item?.name}
           </Link>
         ))}
       </div>
       <div>
-        <h6 className="font-bold mb-6">GET IN TOUCH</h6>
+        <h6 className="font-bold !text-white mb-6">GET IN TOUCH</h6>
         <div className="mb-4">
-          <p className="text-[13px] font-bold mb-2">Coporate Head Office</p>
-          <p className="text-sm leading-6">
-            12 Daniel Yakwo close, 69(A)Road Gawrimpa II Estate, Federal Capital
-            Territory, Abuja.
+          <p className="text-[13px] !text-white font-bold mb-2">Coporate Head Office</p>
+          <p className="text-[15px] font-normal leading-6">
+            12 Daniel Yakwo close, 69(A)Road Gawrimpa II Estate, Federal Capital Territory, Abuja.
           </p>
-          <p className="text-sm mt-3">Tel: +234 818 152 7676</p>
+          <p className="text-[15px] font-normal mt-3">Tel: +234 818 152 7676</p>
         </div>
         <div className="mb-4">
-          <p className="text-[13px] font-bold mb-2">Operations Office</p>
-          <p className="text-sm leading-6">
-            Plot 42C, Road 22, Federal Housing Estate, Rumueme (Agip), Mile IV,
-            P.O.Box 12450, Port Harcourt, Rivers State, Nigeria.
+          <p className="text-[13px] !text-white font-bold mb-2">Operations Office</p>
+          <p className="text-[15px] font-normal leading-6">
+            Plot 42C, Road 22, Federal Housing Estate, Rumueme (Agip), Mile IV, P.O.Box 12450, Port
+            Harcourt, Rivers State, Nigeria.
           </p>
-          <p className="text-sm mt-3">Tel: +234 818 152 7676</p>
+          <p className="text-[15px] font-normal mt-3">Tel: +234 818 152 7676</p>
         </div>
-        <p className="text-[13px] font-bold mb-2">Email Address</p>
-        <p className="text-sm">
-          Email:{' '}
-          <Link
-            href={'mailto:info@weamandcompany.com'}
-            className="hover:underline"
-          >
+        <p className="text-[13px] !text-white font-bold mb-2">Email Address</p>
+        <p className="text-[15px] font-normal">
+          Email:{" "}
+          <Link href={"mailto:info@weamandcompany.com"} className="hover:underline">
             info@weamandcompany.com
           </Link>
         </p>
