8f9ed3ddfe18722ecb922bb0743793fc3e829b6a
logic <afuwapesunday12@gmail.com>
2023-08-24 23:00:13 +0100

    fix

--- public/mailer/mailer.php
+++ public/mailer/mailer.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
 
